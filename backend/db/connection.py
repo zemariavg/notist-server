3480fd3d987531a8691bfb1d7f8bf56ef47cc9fb
zemariavg <zemariavg@gmail.com>
2024-12-13 21:58:56 +0000

    fix env variables

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -7,14 +7,14 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 MYSQL_DB = os.getenv("MYSQL_DB")
 MYSQL_USER = os.getenv("MYSQL_USER")
-MYSQL_PSW = os.getenv("MYSQL_PSW")
+MYSQL_PWD = os.getenv("MYSQL_PWD")
 
 
 def get_db_connection():
     return mysql.connector.connect(
         host=DB_HOST,
         user=MYSQL_USER,
-        password=MYSQL_PSW,
+        password=MYSQL_PWD,
         database=MYSQL_DB,
         port=DB_PORT
     )
