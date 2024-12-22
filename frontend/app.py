41b6637b45e076cc827361002b76e3872e503ca2
Henrique Chan Carvalho Machado <henriqueccmachado@tecnico.ulisboa.pt>
2024-12-22 16:35:52 +0000

    Implemented HTTPS secure channel

--- frontend/app.py
+++ frontend/app.py
@@ -5,21 +5,30 @@ from flask import Flask, jsonify, request, abort, make_response
 from werkzeug.exceptions import HTTPException
 from dotenv import load_dotenv
 from utils.validators import validate_note
+from utils.tls import get_p12_data
 
 load_dotenv()
 BACKEND_URL = os.getenv("BACKEND_URL")
 FE_HOST = os.getenv("FE_HOST")
 FE_PORT = os.getenv("FE_PORT")
-SERVER_TIMEOUT = 10 # seconds
+SERVER_TIMEOUT = 60 # seconds
+P12_PATH = os.getenv("P12_PATH")
+P12_PWD = os.getenv("P12_PWD")
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
+fe_cert, fe_key, be_cert = get_p12_data(P12_PATH, P12_PWD)
+ssl_context = (fe_cert, fe_key)
+session = requests.Session()
+session.cert = (fe_cert, fe_key)
+session.verify = fe_cert
 
 @app.route('/api/users/<username>', methods=['GET'])
 def api_get_user(username):
+    app.logger.info(f"Received user info req from client: {request.remote_addr}")
     try:
-        response = requests.get(f"{BACKEND_URL}/users/{username}")
+        response = session.get(f"{BACKEND_URL}/users/{username}", timeout=SERVER_TIMEOUT)
 
         if response.status_code == 404:
             abort(404, description=response.json().get('error', 'User not found'))
@@ -36,7 +45,7 @@ def backup_note():
         validate_note(note)
         app.logger.info(f"Received note backup req from client: {note['server_metadata']['req_from']}@{request.remote_addr}")
 
-        response = requests.post(f"{BACKEND_URL}/note", json=note, timeout=SERVER_TIMEOUT)
+        response = session.post(f"{BACKEND_URL}/note", json=note, timeout=SERVER_TIMEOUT)
         
         if response.status_code == 403:
             app.logger.error(f"User not authorized to edit note")
@@ -66,4 +75,5 @@ if __name__ == "__main__":
     # logging.basicConfig(filename='frontend.log', level=logging.INFO)
     logging.basicConfig(level=logging.INFO)
     app.logger.info("Starting frontend server")
-    app.run(host=FE_HOST, port=FE_PORT)
+    
+    app.run(host=FE_HOST, port=FE_PORT, ssl_context=ssl_context)
