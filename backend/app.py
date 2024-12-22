3cb23d3f001aeeb221c8d5fdc52156d58a512483
Henrique Chan Carvalho Machado <henriqueccmachado@tecnico.ulisboa.pt>
2024-12-22 16:38:18 +0000

    Implemented HTTPS secure channel

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import requests
 from dotenv import load_dotenv
 
 from flask import Flask, abort, request, make_response
@@ -9,26 +10,32 @@ from werkzeug.exceptions import HTTPException
 
 from db.connection import get_db_session
 from helpers.note_helper import handle_note_upsert
+from db.queries import get_user_by_username
+from utils.tls import get_p12_data
 
 load_dotenv()
 BE_HOST = os.getenv("BE_HOST")
 BE_PORT = os.getenv("BE_PORT")
+P12_PATH = os.getenv("P12_PATH")
+P12_PWD = os.getenv("P12_PWD")
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 
-# @app.route('/users/<username>', methods=['GET'])
-# def get_user(username):
-#     try:
-#         with next(get_db_session()) as session:
-#             user = get_user_by_username(session, username)
-            
-#             if user is None:
-#                 abort(404, description="User not found")
+@app.route('/users/<username>', methods=['GET'])
+def get_user(username):
+    app.logger.info(f"Received user info req from client: {request.remote_addr}")
+    try:
+        with next(get_db_session()) as session:
+            user = get_user_by_username(session, username)
+            app.logger.info(f"User found: {user}")
+            if user is None:
+                abort(404, description="User not found")
                 
-#             return jsonify(user.__repr__()), 200
-#     except Exception as e:
-#         return jsonify({"error": str(e)}), 500
+            return make_response({"user": user.__repr__()}, 200)
+    except Exception as e:
+        app.logger.error(f"Internal server error: {str(e)}")
+        return make_response({"error": str(e)}, 500)
 
 @app.route('/note', methods=['POST'])
 def backup_note():
@@ -40,12 +47,12 @@ def backup_note():
             app.logger.error("Invalid input: No JSON data received")
             abort(400, description="Invalid input: No JSON data received")
 
-        with next(get_db_session()) as session:
+        with next(get_db_session()) as dbsession:
             try:
-                note_id = handle_note_upsert(session, note_data, app.logger)
-                session.commit()
+                note_id = handle_note_upsert(dbsession, note_data, app.logger)
+                dbsession.commit()
             except SQLAlchemyError:
-                session.rollback()
+                dbsession.rollback()
                 raise
 
         return make_response({"Note saved successfully": note_id}, 201)
@@ -64,7 +71,16 @@ def backup_note():
         
 
 if __name__ == "__main__":
-    # logging.basicConfig(filename='backend.log', level=logging.INFO)
+    logging.basicConfig(filename='backend.log', level=logging.INFO)
     logging.basicConfig(level=logging.INFO)
     app.logger.info("Starting backend server")
-    app.run(host=BE_HOST, port=BE_PORT)
+    
+    app.logger.info("Loading server certificates for tls")
+    be_cert, be_key, fe_cert = get_p12_data(P12_PATH, P12_PWD)
+    ssl_context = (be_cert, be_key)
+    session = requests.Session()
+    session.cert = (be_cert, be_key)
+    session.verify = fe_cert
+    app.logger.info("Certificates loaded successfully")
+
+    app.run(host=BE_HOST, port=BE_PORT, ssl_context=ssl_context)
---
+++ backend/utils/tls.py
@@ -0,0 +1,30 @@
+import tempfile
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import pkcs12
+
+def get_p12_data(p12_path: str, p12_pwd: str) -> tuple[str, str, str]:
+    private_key, certificate, other_certs = pkcs12.load_key_and_certificates(
+        open(p12_path, 'rb').read(), 
+        p12_pwd.encode(), 
+        default_backend()
+    )
+
+    with tempfile.NamedTemporaryFile(delete=False) as key_file, \
+         tempfile.NamedTemporaryFile(delete=False) as cert_file:
+
+        key_file.write(private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption(),
+        ))
+        key_file.flush()
+
+        cert_file.write(certificate.public_bytes(serialization.Encoding.PEM))
+        cert_file.flush()
+        
+    with tempfile.NamedTemporaryFile(delete=False) as other_certs_file:
+        other_certs_file.write(other_certs[0].public_bytes(serialization.Encoding.PEM))
+
+    return (cert_file.name, key_file.name, other_certs_file.name)
+    
